8b53bdf9e33774580db39e1978e04041b45e8414
Vikas Bahuguna <vikasedu10@gmail.com>
2021-04-06 18:09:03 +0530

    Add Scaffold body

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,14 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         appBar: AppBar(
           title: Text("I am Rich"),
-          backgroundColor: Colors.pink,
+          backgroundColor: Colors.pink[900],
+        ),
+        backgroundColor: Colors.grey[400],
+        body: Center(
+          child: Image(
+            image: NetworkImage(
+                'https://thumbor.forbes.com/thumbor/fit-in/416x416/filters%3Aformat%28jpg%29/https%3A%2F%2Fspecials-images.forbesimg.com%2Fimageserve%2F5f47d4de7637290765bce495%2F0x0.jpg%3Fbackground%3D000000%26cropX1%3D1398%26cropX2%3D3908%26cropY1%3D594%26cropY2%3D3102'),
+          ),
         ),
       ),
     );
