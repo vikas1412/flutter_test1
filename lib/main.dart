40ab5cb06ee2cd404d0f69aaa6f94f986146ea68
Vikas Bahuguna <vikasedu10@gmail.com>
2021-04-07 09:03:14 +0530

    Add AssetImage rather than NetworkImage

--- lib/main.dart
+++ lib/main.dart
@@ -14,8 +14,7 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.grey[400],
         body: Center(
           child: Image(
-            image: NetworkImage(
-                'https://thumbor.forbes.com/thumbor/fit-in/416x416/filters%3Aformat%28jpg%29/https%3A%2F%2Fspecials-images.forbesimg.com%2Fimageserve%2F5f47d4de7637290765bce495%2F0x0.jpg%3Fbackground%3D000000%26cropX1%3D1398%26cropX2%3D3908%26cropY1%3D594%26cropY2%3D3102'),
+            image: AssetImage('images/elon.jpg'),
           ),
         ),
       ),
